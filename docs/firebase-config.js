312c7b71d22e1025ba488e49e3349939d58ea391
ksilent1337-lgtm <ksilent1337@gmail.com>
2026-04-22 13:47:27 +0300

    Pages: update docs/firebase-config.js

---
+++ docs/firebase-config.js
@@ -0,0 +1,13 @@
+export const firebaseWebConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+export const firebaseOptions = {
+  functionsRegion: "us-central1",
+  telegramUsername: "Dof_white"
+};
